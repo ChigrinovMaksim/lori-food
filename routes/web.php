fcb5be07c6c9e8b2315512dd6a453b89cbfafedb
Кирилл Чигринов <kirill.chigrinov@gmail.com>
2020-04-05 20:10:06 +0300

    Add telegram

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 Route::get('/', 'Controller@getMainPage');
 Route::get('/pizza-city', 'Controller@getPizzaCity');
 Route::get('/fountain', 'Controller@getFountain');
+Route::post('/callback', 'Controller@postAjaxCallback');
 
