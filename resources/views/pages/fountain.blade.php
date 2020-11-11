921b1116403e8f728f19fd0a6d585c7f3da99ede
ChigrinovMaks <chigrinovmaksim1991@gmail.com>
2020-11-11 14:52:26 +0200

    Changed new fountain menu photo

--- resources/views/pages/fountain.blade.php
+++ resources/views/pages/fountain.blade.php
@@ -19,37 +19,37 @@
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/breakfast-salad.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/1.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/main-dishes.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/2.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/snacks.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/3.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/kebab-hot-drinks.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/4.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/alcohol-bear-snacks.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/5.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
             <div class="body_restaurant_fountain">
                 <div class="fountain_restaurant_img">
-                    <img src="images/fountain/alcohol-soup.jpg" class="fountain_restaurant_image">
+                    <img src="images/fountain/6.jpg" class="fountain_restaurant_image">
                 </div>
             </div>
 
